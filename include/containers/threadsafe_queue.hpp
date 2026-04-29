c9e3b6740c45b6e7f76e86b50070f1c44d6211b2
Vladislav Isaychev <gregnov69@gmail.com>
2026-04-29 21:18:10 +0300

    updated .gitignore; eliminated data race in head_ == tail;

--- include/containers/threadsafe_queue.hpp
+++ include/containers/threadsafe_queue.hpp
@@ -1,6 +1,7 @@
 #ifndef THREADSAFE_QUEUE_HPP
 #define THREADSAFE_QUEUE_HPP
 
+#include <algorithm>
 #include <memory>
 #include <mutex>
 
@@ -19,71 +20,65 @@ namespace mt
     };
 
   public:
-    threadsafe_queue() :
-      dummy_head_(std::make_unique< node >()), tail_(dummy_head_.get())
+    threadsafe_queue() : head_(std::make_unique< node >()), tail_(head_.get())
     {}
-    // threadsafe_queue(const threadsafe_queue& rhs) = delete;
-    // threadsafe_queue(threadsafe_queue&& other) {}
-    // threadsafe_queue& operator=(const threadsafe_queue& rhs) = delete;
-    // threadsafe_queue& operator=(threadsafe_queue&& other) {}
+
+    threadsafe_queue(const threadsafe_queue& rhs) = delete;
+    threadsafe_queue(threadsafe_queue&& other) = delete;
+
+    threadsafe_queue& operator=(const threadsafe_queue& rhs) = delete;
+    threadsafe_queue& operator=(threadsafe_queue&& other) = delete;
 
     void push(T rhs)
     {
-      std::unique_ptr< node > new_node =
-        std::make_unique< node >(std::ref(rhs));
-
+      std::unique_ptr< node > new_dummy = std::make_unique< node >();
       std::lock_guard tail_lock{ tail_mutex_ };
-      tail_->next = std::move(new_node);
+
+      tail_->data = std::move(rhs);
+      tail_->next = std::move(new_dummy);
       tail_ = tail_->next.get();
     }
 
-    // std::unique_ptr< T > try_pop() {
-
-    // std::unique_ptr< node > head = try_pop_head();
-    // return head.get() == nullptr ? std::unique_ptr< T >{}
-    //                              : std::make_unique(std::move(head->data));
-    // }
-
     bool try_pop(T& lhs)
     {
-      std::unique_ptr< node > head = try_pop_head();
-      if (head.get() == nullptr)
+      std::unique_ptr< node > popped_head = try_pop_head();
+      if (!popped_head.get())
       {
         return false;
       }
-      lhs = std::move(head->data);
+      lhs = std::move(popped_head->data);
       return true;
     }
 
     bool empty() const
     {
-      std::scoped_lock lock{ head_mutex_, tail_mutex_ };
-      return tail_ == dummy_head_.get();
+      std::lock_guard lock{ head_mutex_ };
+      return head_.get() == get_tail();
     }
 
   private:
-    std::unique_ptr< node > dummy_head_;
+    std::unique_ptr< node > head_;
     std::mutex head_mutex_;
 
     node* tail_;
     std::mutex tail_mutex_;
 
+    node* get_tail()
+    {
+      std::lock_guard tail_lock{ tail_mutex_ };
+      return tail_;
+    }
+
     std::unique_ptr< node > try_pop_head()
     {
       std::lock_guard head_lock{ head_mutex_ };
-      if (dummy_head_.get() == tail_)
+      if (head_.get() == get_tail())
       {
         return std::unique_ptr< node >{};
       }
 
-      std::lock_guard tail_lock{ tail_mutex_ };
-      std::unique_ptr< node > tmp = std::move(dummy_head_->next);
-      dummy_head_->next = std::move(tmp->next);
-
-      if (tmp.get() == tail_)
-      {
-        tail_ = dummy_head_.get();
-      }
+      std::unique_ptr< node > tmp = std::move(head_);
+      head_ = std::move(tmp->next);
       return tmp;
     }
   };
